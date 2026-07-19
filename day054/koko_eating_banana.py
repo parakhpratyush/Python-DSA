e9c9a76fd484cdf025cff608065cadda09b41363
parakhpratyush <parakhpratyush@gmail.com>
2026-07-19 21:19:59 +0530

    Day 54 : Koko Eating Bananas.

--- day054/koko_eating_banana.py
+++ day054/koko_eating_banana.py
@@ -19,3 +19,10 @@ class Solution(object):
                 left = mid + 1
                 
         return left
+    
+#----Testing----
+solver = Solution()
+
+print(solver.minEatingSpeed(piles = [3,6,7,11], h = 8))
+print(solver.minEatingSpeed(piles = [30,11,23,4,20], h = 5))
+print(solver.minEatingSpeed(piles = [30,11,23,4,20], h = 6))
